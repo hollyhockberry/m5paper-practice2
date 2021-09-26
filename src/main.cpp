0b1982deb10181da42616927b1a093768a0fe65d
Inaba <49336378+hollyhockberry@users.noreply.github.com>
2021-09-26 13:29:09 +0900

    Update main.cpp
    upgrade M5GFX to 0.0.7

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,106 @@
-#include <SPI.h>
-#include "FS.h"
-#include "SD.h"
+// Copyright (c) 2021 Inaba
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+
+#include <vector>
+#include <M5EPD.h>
 #include <M5GFX.h>
 
-/*
-
-/img のファイルをSDカードのルートにコピーします。
-inaba.bmp 540x540 24bit 
-inaba2.bmp 270x270 24bit
-
-ボタン G37で /inaba.bmp, G39で /inaba2.bmp で createFromBmpFile()します。
-
-再現方法
-* inaba.bmpだけ何度もcreateFromBmpFileしてもabort()はしない
-* inaba2.bmpを開いた後だとどのファイルを開いてもabort()する
-
-*/
+extern const uint8_t rgb888[];
 
 namespace {
 
-void drawImage(const String& path, M5Canvas& canvas) {
-  Serial.printf("open: %s\r\n", path.c_str());
-  canvas.deleteSprite();
-  canvas.deletePalette();
-  if (path.endsWith("bmp") || path.endsWith("BMP")) {
-    canvas.createFromBmpFile(SD, path.c_str());
-  } else {
-    // TODO
+void listFiles(std::vector<String>& files, File& file) {
+  while (true) {
+    File entry = file.openNextFile();
+    if (!entry) {
+      return;
+    }
+    if (entry.isDirectory()) {
+      // ignore dot directories
+      if (entry.name()[1] != '.') {
+        listFiles(files, entry);
+      }
+    } else {
+      files.push_back(String(entry.name()));
+    }
+    entry.close();
   }
-  Serial.printf("(%d, %d)\r\n", canvas.width(), canvas.height());
-  canvas.pushSprite(0, 300);
+}
+
+
+// Draw a file list on Canvas.
+// INFO: Cannot display too many files :-)
+void drawFilelist(std::vector<String>& files, int index, M5GFX& display) {
+  display.setEpdMode(epd_mode_t::epd_fastest);
+  M5Canvas canvas(&display);
+  canvas.setPsram(true);
+  canvas.createSprite(540, 300);
+  canvas.setTextSize(3);
+  canvas.clear(TFT_WHITE);
+
+  int i = 0;
+  for (const auto& file : files) {
+    if (index == i) {
+      canvas.fillRect(0, 8 + i * 32, 540, 32, TFT_BLACK);
+      canvas.setTextColor(TFT_WHITE, TFT_BLACK);
+    } else {
+      canvas.setTextColor(TFT_BLACK, TFT_WHITE);
+    }
+    canvas.drawString(file.c_str(), 10, 10 + i * 32);
+    ++i;
+  }
+  canvas.pushSprite(0, 0);
+}
+
+
+void drawImage(const String& path, M5GFX& display) {
+  if (!path.endsWith("bmp") && !path.endsWith("BMP")) {
+    return;
+  }
+  display.setEpdMode(epd_mode_t::epd_quality);
+  M5Canvas canvas(&display);
+  canvas.setPsram(true);
+  canvas.createFromBmpFile(SD, path.c_str());
+  const float dx = 350.f / static_cast<float>(canvas.width());
+  const float dy = 350.f / static_cast<float>(canvas.height());
+  canvas.pushRotateZoomWithAA(270, 630, 0.f, dx, dy);
 }
 
 M5GFX display;
-M5Canvas canvas(&display);
+
+std::vector<String> files;
+int fileIndex = 0;
 
 }  // namespace
 
 void setup() {
-  Serial.begin(115200);
-  SPI.begin(14, 13, 12, 4);
-  SD.begin(4, SPI, 20000000);
-
-  ::pinMode(37, INPUT);
-  ::pinMode(39, INPUT);
+  M5.begin();
 
   display.begin();
-  display.setEpdMode(epd_mode_t::epd_quality);
   display.invertDisplay(false);
   display.clear(TFT_WHITE);
   display.setRotation(0);
+
+  File root = SD.open("/");
+  listFiles(files, root);
+
+  drawFilelist(files, fileIndex, display);
 }
 
 void loop() {
-  const char* files[] = { "/inaba.bmp", "/inaba2.bmp" };
+  M5.update();
 
-  if (::digitalRead(37) == LOW) {
-    drawImage(String(files[0]), canvas);
-  } else if (::digitalRead(39) == LOW) {
-    drawImage(String(files[1]), canvas);
+  if (M5.BtnL.wasPressed()) {
+    fileIndex--;
+  } else if (M5.BtnR.wasPressed()) {
+    fileIndex++;
+  } else if (M5.BtnP.wasPressed()) {
+    drawImage(files[fileIndex], display);
+    return;
   } else {
     return;
   }
+  fileIndex = constrain(fileIndex, 0, files.size() - 1);
+  drawFilelist(files, fileIndex, display);
 }
